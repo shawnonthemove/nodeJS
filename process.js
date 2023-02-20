0571df0927fcb068835c592fd66ae4e389a059af
shawnonthemove <1026187367@qq.com>
2023-02-20 16:32:22 +0800

    feat: 进程实例

---
+++ process.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const [url, port] = ['127.0.0.1', 3000]
+
+function computation() {
+  let sum = 0;
+  console.info('计算开始');
+  console.time('计算耗时');
+
+  for(let i = 0; i < 1e10; i++) {
+    sum += i;
+  }
+
+  console.info('计算结束');
+  console.timeEnd('计算耗时');
+  return sum;
+}
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/compute') {
+    const sum = computation();
+    res.end(`Sum is ${sum}`);
+  }
+  else res.end(`OK`);
+})
+
+server.listen(port, url, () => {
+  console.log('listening on port ' + port);
+})
